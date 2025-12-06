cf15aa16cd36d4b92473ca9f226fe3590a23861e
heterogeneous riders <heterogeneousriders@gmail.com>
2025-12-06 09:02:51 +0530

    Add project files.

---
+++ validators/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using validators.Models;
+
+namespace validators.Controllers
+{
+    public class StudentController : Controller
+    {
+        private static List<StudentViewModel> students = new List<StudentViewModel>();
+        public ActionResult Index()
+        {
+            return View(students);
+        }
+        public IActionResult Create()
+        {
+            LoadDropDown();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(StudentViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                students.Add(model);
+                return RedirectToAction("Index");
+            }
+            LoadDropDown();
+            return View();
+        }
+
+        private void LoadDropDown()
+        {
+            ViewBag.Vijay = new List<SelectListItem>
+            {
+                new SelectListItem {Text = "India", Value="1"},
+                new SelectListItem {Text = "UK", Value="2"},
+                new SelectListItem {Text = "USA", Value="3"}
+            };
+        }
+    }
+}
---
+++ validators/Models/StudentViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace validators.Models;
+
+public class StudentViewModel
+{
+    [Required(ErrorMessage ="Full Name Required")]
+    [StringLength(30,MinimumLength =3, ErrorMessage ="Lengenth must be inbetween 3 and 30")]
+    [Display(Name="Name")]
+    public string FullName { get; set; }
+
+    [Range(18,60,ErrorMessage ="Age must be betwenn 18 and 60")]
+    public int Age { get; set; }
+    [EmailAddress(ErrorMessage ="Enter Valid mail")]
+    public string Email { get; set; }
+
+    [RegularExpression(@"\d{10}$", ErrorMessage ="Enter validNumber 10 digit")]
+    public string Mobile { get; set; }
+
+    [Required]
+    public string Gender { get; set; }
+
+    [Display(Name="country")]
+    public int CountryId { get; set; }
+
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Compare("Password", ErrorMessage ="Password missmatch")]
+    [DataType(DataType.Password)]
+    public string ComparePassword { get; set; }
+
+}
